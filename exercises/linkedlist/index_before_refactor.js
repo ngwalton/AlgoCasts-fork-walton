b148d6e77d54205a83d4b9fca418d45d9efb8c96
Nicholas Walton <ngwalton@gmail.com>
2021-06-20 19:50:21 -0500

    Copied from index.js before refactor

---
+++ exercises/linkedlist/index_before_refactor.js
@@ -0,0 +1,178 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+// Ask if we'll be writing any addional methods if asked to write, e.g.,
+// .getFirst to avoid refactoring/unneeded code.
+
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let node = this.head;
+        let count = 0;
+
+        while (node) {
+            count++;
+            node = node.next;
+        }
+
+        return count;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let node = this.head;
+
+        if (!node) {
+            return null;
+        }
+
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+            node = node.next;
+        }
+
+        return last;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        let previous = this.head;
+
+        if (!previous) {
+            return;
+        }
+
+        let node = previous.next;
+
+        if (!node) {
+            this.head = null;
+            return;
+        }
+
+        while (node.next) {
+            previous = node;
+            node = node.next;
+        }
+
+        previous.next = null;
+    }
+
+    insertLast(data) {
+        const last = this.getLast();
+
+        if (last) {
+            // at least one node in linked list
+            last.next = new Node(data);
+            return;
+        }
+
+        // no nodes in linked list
+        this.head = new Node(data);
+    }
+
+    // insertLast without getLast
+    // insertLast(data) {
+    //     let node = this.head;
+
+    //     if (!node) {
+    //         this.head = new Node(data);
+    //         return;
+    //     }
+
+    //     while (node.next) {
+    //         node = node.next;
+    //     }
+
+    //     node.next = new Node(data);
+    // }
+
+    getAt(index) {
+        let count = 0;
+        let node = this.head;
+
+        while (node) {
+            if (count === index) {
+                return node;
+            }
+
+            node = node.next;
+            count++;
+        }
+
+        return null;
+    }
+
+    removeAt(index) {
+        if (!this.head) {
+            return;
+        }
+
+        if (index === 0) {
+            this.removeFirst();
+            // this.head = this.head.next;
+            return;
+        }
+
+        let previous = this.getAt(index - 1);
+        // could use getAt(index + 1), but could be expensive
+        // previous.next = this.getAt(index + 1);
+        if (!previous || !previous.next) {
+            return;
+        }
+        previous.next = previous.next.next;
+    }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        // handle case index is out of bounds
+        let previous = this.getAt(index - 1) || this.getLast();
+        previous.next = new Node(data, previous.next);
+    }
+}
+
+// let x = new LinkedList();
+// x.insertFirst(1);
+// x.insertFirst(2);
+// x.removeLast();
+
+module.exports = { Node, LinkedList };
