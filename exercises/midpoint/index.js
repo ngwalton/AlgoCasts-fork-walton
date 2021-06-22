6ac7ea0d408a19b56e3391b7b02aaa3646e768b1
Nicholas Walton <ngwalton@gmail.com>
2021-06-21 21:17:17 -0500

    First commit of midpoint

--- exercises/midpoint/index.js
+++ exercises/midpoint/index.js
@@ -2,6 +2,7 @@
 // Return the 'middle' node of a linked list.
 // If the list has an even number of elements, return
 // the node at the end of the first half of the list.
+//// My note: with even length, return "first" middle node
 // *Do not* use a counter variable, *do not* retrieve
 // the size of the list, and only iterate
 // through the list one time.
@@ -12,6 +13,16 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+    let slow = list.getFirst();  // moves forward one step at a time
+    let fast = list.getFirst();  // movers forward two steps at a time
+
+    while (fast.next && fast.next.next) {  // check if fast has somewhere to go
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    return slow;
+}
 
 module.exports = midpoint;
