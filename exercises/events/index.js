de151486531df11ce8edaf7f05ac41f7a642f987
Nicholas Walton <ngwalton@gmail.com>
2021-07-08 18:46:41 -0500

    First commit of events

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,36 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  // each key is an event name, each value is an array of callbacks
+  #events;
+  constructor() {
+    this.#events = Object.create(null);
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.#events[eventName]) {
+      this.#events[eventName].push(callback);
+    } else {
+      this.#events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.#events[eventName]) {
+      for (const fn of this.#events[eventName]) {
+        fn();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.#events[eventName];
+  }
 }
 
 module.exports = Events;
