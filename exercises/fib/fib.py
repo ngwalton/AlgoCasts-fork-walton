4d43089cc0ae7d1a2e16f6507d3bdb409ddbffc7
Nicholas Walton <ngwalton@gmail.com>
2021-07-25 21:18:03 -0500

    Improved handling of edge cases, formatting

--- exercises/fib/fib.py
+++ exercises/fib/fib.py
@@ -3,33 +3,39 @@ import functools
 
 @functools.cache
 def fib(n):
-    if (n < 2):
-        return n
+    if n < 1:
+        return 0
+    elif n == 1:
+        return 1
 
-    return fib(n-2) + fib(n-1)
+    return fib(n-1) + fib(n-2)
+
 
 # hand written memoization
-# def memoize(fn):
+# def memoise(fn):
 #     cache = {}
 
-#     def fnOut(*args):
+#     def fn_out(*args):
 #         if args not in cache:
 #             cache[args] = fn(*args)
 
 #         return cache[args]
 
-#     return fnOut
+#     return fn_out
 
-# fib = memoize(fib)
+
+# fib = memoise(fib)
 
 # iterative
 # def fib(n):
-#     if n < 2:
-#         return n
+#     if n < 1:
+#         return 0
+#     elif n == 1:
+#         return 1
 
 #     result = [0, 1]
 
-#     for i in range(2, n+1):
+#     for _ in range(2, n+1):
 #         result.append(result[-2] + result[-1])
 
 #     return(result[-1])
