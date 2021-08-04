a97ee3a07a824c13ee751e9c411da78eeb40e418
Nicholas Walton <ngwalton@gmail.com>
2021-08-04 15:31:11 -0500

    Use better regex

--- exercises/anagrams/index.py
+++ exercises/anagrams/index.py
@@ -7,7 +7,7 @@ import re
 
 def anagrams(str1: str, str2: str) -> bool:
     def clean(string):
-        string = re.sub(r"[^\w]", "", string.lower())
+        string = re.sub(r"\W", "", string.lower())
         return "".join(sorted(string))
 
     return clean(str1) == clean(str2)
