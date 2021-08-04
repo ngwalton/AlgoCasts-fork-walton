8eb6e9c1e06a8230027669e271bd97720c1f9f75
Nicholas Walton <ngwalton@gmail.com>
2021-08-04 16:07:47 -0500

    Test with odd number of chars

--- exercises/reversestring/test.js
+++ exercises/reversestring/test.js
@@ -11,3 +11,7 @@ test('Reverse reverses a string', () => {
 test('Reverse reverses a string', () => {
   expect(reverse('  abcd')).toEqual('dcba  ');
 });
+
+test('Reverse reverses a string with odd number of chars', () => {
+  expect(reverse('  a bcd')).toEqual('dcb a  ');
+});
