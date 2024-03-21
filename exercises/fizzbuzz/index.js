4026f84ba59ecfc82b6c5b0d088010e83de70060
Nicholas Walton <ngwalton@gmail.com>
2024-03-20 21:40:38 -0500

    Add compact version of fizzBuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -13,26 +13,36 @@
 //   buzz
 
 function fizzBuzz(n) {
-    const fizz = 'fizz';
-    const buzz = 'buzz';
-
     const aIsMultOfN = (a, n) => a % n === 0;
 
     for (let i = 1; i <= n; i++) {
-        const isMult3 = aIsMultOfN(i, 3);
-        const isMult5 = aIsMultOfN(i, 5);
-
-        // this is multiples of 15
-        if (isMult3 && isMult5) {
-            console.log(fizz + buzz);
-        } else if (isMult3) {
-            console.log(fizz);
-        } else if (isMult5) {
-            console.log(buzz);
-        } else {
-            console.log(i);
-        }
+        const fizz = aIsMultOfN(i, 3) ? 'fizz' : '';
+        const buzz = aIsMultOfN(i, 5) ? 'buzz' : '';
+        console.log(fizz + buzz || i);
     }
 }
+
+// function fizzBuzz(n) {
+//     const fizz = 'fizz';
+//     const buzz = 'buzz';
+
+//     const aIsMultOfN = (a, n) => a % n === 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         const isMult3 = aIsMultOfN(i, 3);
+//         const isMult5 = aIsMultOfN(i, 5);
+
+//         // this is multiples of 15
+//         if (isMult3 && isMult5) {
+//             console.log(fizz + buzz);
+//         } else if (isMult3) {
+//             console.log(fizz);
+//         } else if (isMult5) {
+//             console.log(buzz);
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
 
 module.exports = fizzBuzz;
